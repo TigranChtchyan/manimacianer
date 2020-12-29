1f7ee0bd2947cfaaa7a07c827fe4ec40594bc240
Tigran Chtchyan <76706739+TigranChtchyan@users.noreply.github.com>
2020-12-29 22:21:19 +0400

    Update Useful_Functions.py

--- Useful_Functions.py
+++ Useful_Functions.py
@@ -4,7 +4,7 @@
 from manimlib.imports import *
 import numpy as np
 
-#vdaaaaaaaaaa
+# Այս ֆունկցիան վերադարձնում է
 def vernagir(header):
 	lin=Line(5*LEFT, 5*RIGHT)
 	vern=VGroup(header, lin)
@@ -12,25 +12,27 @@ def vernagir(header):
 	vern.to_edge(UP)
 	return vern
   
-#ervrrrrrrrrre
-def qarakusikner(w,n,color):
+# Այս ֆունկցիան վերադարձնում է width երկարությամբ կողմ ունեցող և color գույն ունեցող
+# կողք կողքի շարված n հատ քառակուսիկների Group, որի կենտրոնը (0,0) կետն է
+def qarakusikner(width,n,color):
 	x=Square(color=color)
-	x.set_width(w)
+	x.set_width(width)
 	for i in range(1,n):
 		s=Square(color=color)
-		s.set_width(w)
-		s.shift((i*w)*RIGHT)
+		s.set_width(width)
+		s.shift((i*width)*RIGHT)
 		x=VGroup(x, s)
-	x.shift((n-1)*w*0.5*LEFT)
+	x.shift((n-1)*width*0.5*LEFT)
 	return x
-  
-#vsdddddddddddddddddd
-def aghyusak(w, togh, syun, color):
-	t=(qarakusikner(w, syun, color))
-	a=t
+
+# Այս ֆունկցիան վերադարձնում է width երկարությամբ կողմ ունեցող և color գույն ունեցող
+# togh*syun աղյուսակի տեսքով շարված քառակուսիկների Group, որի կենտրոնը (0,0) կետն է
+def aghyusak(width, togh, syun, color):
+	t=(qarakusikner(width, syun, color))
+	a=t.copy()
 	for i in range(1,togh):
 		s=a.copy()
-		s.shift((i*w)*UP)
+		s.shift((i*width)*UP)
 		t=VGroup(t, s)
-	t.shift((togh-1)*w*0.5*DOWN)
+	t.shift((togh-1)*width*0.5*DOWN)
 	return t
